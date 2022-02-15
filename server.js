de80f9d2c12df66a005f1bd0f8477e9d8128d521
NBS5000 <steve.nbs.barry@gmail.com>
2022-02-15 14:27:13 +1100

    folder structure set up

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const path = require('path');
+
+
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+// Middleware *****
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
+
+
+// Routes *****
+
+
+// GET Route for homepage
+app.get('/', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+
+
+
+app.listen(PORT, () =>
+    console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
