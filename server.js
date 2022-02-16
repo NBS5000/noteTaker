12c3ef48fb7cebc3f71692ac653c9357fe9b99d1
NBS5000 <steve.nbs.barry@gmail.com>
2022-02-16 21:19:10 +1100

    routes to index and notes now work, uuid generator added

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 
-
+const uuid = require('./helpers/uuid');
 
 const PORT = process.env.PORT || 3001;
 
@@ -11,6 +11,11 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api', api);
+// app.use(express.static('public'));
+
+
+
+
 
 
 // Routes *****
@@ -21,6 +26,10 @@ app.get('/', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
+// GET Route for notes page
+app.get('/notes', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
 
 
 
